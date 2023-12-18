97917391d92cefa771a08cf345a9d0871c96aed9
Mikhail-Mishchukov <mi.mishchukov@gmail.com>
2023-12-19 00:23:43 +0500

    Continue exploring StyleX

--- StyleX/src/App.tsx
+++ StyleX/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import stylex from "@stylexjs/stylex";
 
-const styles = stylex.create({
+const containerStyles = stylex.create({
   container: {
     display: "flex",
     flexDirection: "column",
@@ -10,46 +10,128 @@ const styles = stylex.create({
     alignItems: "center",
     width: "100%",
     height: "100vh",
-    backgroundColor: "black",
   },
+});
+
+const buttonStyles = stylex.create({
   buttonReset: {
     backgroundColor: "transparent",
     border: "none",
     cursor: "pointer",
   },
   button: {
-    padding: "12px 16px",
-    borderRadius: 4,
-    background: {
-      default: "#00D1B2",
-      ":hover": "#00B89C",
-      ":focus": "#00D1B2",
-      ":active": "#00D1B2",
-    },
-    border: "1px solid #00D1B2",
-    borderColor: {
-      default: "#00D1B2",
-      ":focus": "#D3FFF8",
-    },
-    color: "white",
+    padding: "17px 35px",
+    border: "1px solid  #9873FF",
+    fontFamily: "'Open Sans'",
+    fontSize: 20,
+    fontWeight: 500,
   },
-  highlighted: {
-    color: "rebeccapurple",
+  primary: {
+    color: "white",
+    backgroundColor: "#9873FF",
+    ":hover": {
+      backgroundColor: "#B89EFF",
+      borderColor: "#B89EFF",
+    },
+    ":active": {
+      backgroundColor: "#8052FF",
+      borderColor: "#8052FF",
+    },
+    ":disabled": {
+      backgroundColor: "#C8C5D1",
+      borderColor: "#C8C5D1",
+      ":hover": {
+        backgroundColor: "#C8C5D1",
+        borderColor: "#C8C5D1",
+      },
+      ":active": {
+        backgroundColor: "#C8C5D1",
+        borderColor: "#C8C5D1",
+      },
+    },
+  },
+  secondary: {
+    color: "#9873FF",
+    backgroundColor: "white",
+    ":hover": {
+      color: "white",
+      backgroundColor: "#B89EFF",
+    },
+    ":active": {
+      color: "white",
+      backgroundColor: "#8052FF",
+    },
+    ":disabled": {
+      color: "#C8C5D1",
+      backgroundColor: "white",
+      borderColor: "#C8C5D1",
+      ":hover": {
+        color: "#C8C5D1",
+        backgroundColor: "white",
+        borderColor: "#C8C5D1",
+      },
+      ":active": {
+        color: "#C8C5D1",
+        backgroundColor: "white",
+        borderColor: "#C8C5D1",
+      },
+    },
   },
 });
 
+const inputStyles = stylex.create({
+  resetInputStyles: {
+    border: "unset",
+    ":focus": {
+      outline: "unset",
+    },
+  },
+  baseStyles: {
+    width: {
+      default: 300,
+      "@media (max-width: 1000px)": 200,
+    },
+    padding: "15px 20px",
+    borderRadius: 6,
+    "::placeholder": {
+      color: "#1D2433",
+    },
+  },
+});
+
+type ButtonVariant = "primary" | "secondary";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [buttonVariant, setButtonVariant] = useState<ButtonVariant>("primary");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   return (
-    <div {...stylex.props(styles.container)}>
-      <div>{count}</div>
+    <div {...stylex.props(containerStyles.container)}>
       <button
-        {...stylex.props(styles.buttonReset, styles.button)}
-        onClick={() => setCount((prev) => prev + 1)}
+        disabled={isButtonDisabled}
+        {...stylex.props(
+          buttonStyles.buttonReset,
+          buttonStyles.button,
+          buttonStyles[buttonVariant]
+        )}
+        onClick={() => {
+          if (buttonVariant === "primary") {
+            setButtonVariant("secondary");
+            return;
+          }
+          setButtonVariant("primary");
+        }}
       >
-        Increase
+        Change button {buttonVariant}
       </button>
+      <button onClick={() => setIsButtonDisabled((prev) => !prev)}>
+        Disable button above
+      </button>
+
+      <input
+        {...stylex.props(inputStyles.resetInputStyles, inputStyles.baseStyles)}
+        placeholder="Placeholder"
+      ></input>
     </div>
   );
 }
